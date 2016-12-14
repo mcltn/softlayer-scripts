d9938031cddfdbed7750196d509f29b16fb68754
mcltn <matt.colton@gmail.com>
2016-12-14 09:52:53 -0600

    Added removal of initial gateway

--- burst/post-executer.ps1
+++ burst/post-executer.ps1
@@ -27,7 +27,7 @@ $domain_user = "testaccount"
 $domain_ou_path = "OU=CloudServers,DC=dev"
 $dns_servers = "172.16.0.11" #"172.16.0.11,10.0.80.11,10.0.80.12"
 $gateway = "172.16.0.1"
-
+$remove_gateway = "172.20.130.1" # $null
 
 $domain_user_key = New-Object Byte[] 16   # You can use 16, 24, or 32 for AES
 [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($domain_user_key)
@@ -125,6 +125,10 @@ foreach($h in $config.hosts){
             if (!$?)
             {
                 " Error saving IP Address "
+            }
+
+            if ($remove_gateway) {
+                $private_nic | Remove-NetRoute NextHop $remove_gateway
             }
 
             Disable-NetAdapterBinding -Name "PrivateNetwork-A" -ComponentID ms_tcpip6
